66ed4c49184927dcbff9fe386823944d706fd88c
viktor95801 <155126585+Viktor95801@users.noreply.github.com>
2025-07-31 01:12:06 -0300

    Experiment: what is causing it to break on mobile? fetch(), marked.js or .innerHTML?

--- main.js
+++ main.js
@@ -22,12 +22,25 @@ const getFileContent = async (file_name) => {
         });
 }
 
-let main_page;
-if(file_map[browser_lang] != null) {
-    main_page = await getFileContent(file_map[browser_lang]);
-} else {
-    main_page = await getFileContent(file_map['en']);
-}
+// let main_page;
+// if(file_map[browser_lang] != null) {
+//     main_page = await getFileContent(file_map[browser_lang]);
+// } else {
+//     main_page = await getFileContent(file_map['en']);
+// }
+
+let main_page = `# Bem-vindo  
+
+Este é meu site, aqui encontrará meu blog (com artigos) e outras coisas aleatórias que eu gostaria de aprender.
+Este site é feito para ajudar-me ao aprender e espero que vocês também aprendam com seu código.
+
+## O que posso fazer aqui?
+
+1. Bom, este é o hub para interagir com meu blog. [Você pode ver meu blog aqui](/blog/).
+
+## Código fonte!! 💢
+
+Pode encontrar o código fonte [disponível aqui](https://github.com/Viktor95801/viktor95801.github.io).`
 
 content = marked.parse(main_page);
 document.getElementById('content').innerHTML = content;
